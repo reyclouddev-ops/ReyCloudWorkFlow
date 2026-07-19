dadbdc8e374c26e9fac54e6e1d8b46c79474aa3a
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-19 22:05:25 +0700

    Update repo.js

--- routes/repo.js
+++ routes/repo.js
@@ -113,3 +113,128 @@ err.response?.data
 
 
 });
+
+// ======================
+// Create Repository
+// ======================
+
+
+router.post(
+"/create",
+async(req,res)=>{
+
+
+try{
+
+
+const {
+
+name,
+
+privateRepo
+
+}=req.body;
+
+
+
+if(!req.session.user){
+
+return res.json({
+
+success:false,
+
+message:
+"Github belum connect"
+
+});
+
+}
+
+
+
+
+
+const repo =
+await axios.post(
+
+"https://api.github.com/user/repos",
+
+{
+
+name,
+
+private:
+privateRepo || false,
+
+
+auto_init:true
+
+
+},
+
+{
+
+headers:{
+
+Authorization:
+`Bearer ${req.session.user.token}`,
+
+Accept:
+"application/vnd.github+json"
+
+}
+
+}
+
+);
+
+
+
+
+
+res.json({
+
+success:true,
+
+repo:{
+
+
+name:
+repo.data.name,
+
+
+url:
+repo.data.html_url
+
+
+}
+
+});
+
+
+
+
+}
+
+
+catch(err){
+
+
+res.json({
+
+success:false,
+
+error:
+err.response?.data
+
+});
+
+
+}
+
+
+});
+
+
+
+module.exports=router;
