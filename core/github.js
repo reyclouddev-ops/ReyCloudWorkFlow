042b0122ff8faa2723b91e36d2377bdfe66f36df
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-19 22:41:28 +0700

    Update github.js

--- core/github.js
+++ core/github.js
@@ -1,81 +1,30 @@
 const axios = require("axios");
-const fs = require("fs");
-const path = require("path");
 
 
 
-async function uploadFile(
+
+
+// ==========================
+// CREATE REPOSITORY
+// ==========================
+
+
+async function createRepo(
 token,
-owner,
-repo,
-filePath,
-githubPath
+name
 ){
 
 
+const res =
+await axios.post(
 
-const content =
-fs.readFileSync(
-filePath
-)
-.toString("base64");
-
-
-
-try{
-
-
-// cek file sudah ada atau belum
-
-let sha;
-
-
-
-try{
-
-
-const old =
-await axios.get(
-
-`https://api.github.com/repos/${owner}/${repo}/contents/${githubPath}`,
+"https://api.github.com/user/repos",
 
 {
 
-headers:{
+name,
 
-Authorization:
-`Bearer ${token}`
-
-}
-
-}
-
-);
-
-
-sha =
-old.data.sha;
-
-
-}
-
-catch(e){}
-
-
-
-
-await axios.put(
-
-`https://api.github.com/repos/${owner}/${repo}/contents/${githubPath}`,
-
-{
-
-message:
-"🚀 Upload from ReyDeploy",
-
-content,
-
-sha
+private:false
 
 },
 
@@ -97,25 +46,7 @@ Accept:
 
 
 
-return true;
-
-
-
-}
-
-catch(err){
-
-
-console.log(
-err.response?.data || err
-);
-
-
-return false;
-
-
-}
-
+return res.data;
 
 
 }
@@ -123,84 +54,81 @@ return false;
 
 
 
-// Scan semua file
 
-function scanFolder(
-folder,
-base=""
+
+
+
+// ==========================
+// UPLOAD FILE
+// ==========================
+
+
+async function uploadFile(
+
+token,
+
+owner,
+
+repo,
+
+filePath,
+
+content
+
 ){
 
 
-let files=[];
+
+const url =
+
+`https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`;
 
 
 
-const list =
-fs.readdirSync(
-folder
-);
+const res =
+await axios.put(
+
+url,
+
+{
+
+message:
+"ReyDeploy upload",
 
 
+content:
 
-for(
-const item of list
-){
-
-
-const full =
-path.join(
-folder,
-item
-);
-
-
-
-const target =
-path.join(
-base,
-item
-);
-
-
-
-if(
-fs.statSync(full).isDirectory()
-){
-
-
-files.push(
-...scanFolder(
-full,
-target
+Buffer.from(
+content
+).toString(
+"base64"
 )
+
+},
+
+{
+
+headers:{
+
+Authorization:
+`Bearer ${token}`,
+
+Accept:
+"application/vnd.github+json"
+
+}
+
+}
+
 );
 
 
-}
 
-else{
-
-
-files.push({
-
-local:full,
-
-github:target.replaceAll("\\","/")
-
-});
+return res.data;
 
 
 }
 
-
-}
-
-
-
-return files;
-
-
-}
 
 
 
@@ -208,8 +136,8 @@ return files;
 
 module.exports={
 
-uploadFile,
+createRepo,
 
-scanFolder
+uploadFile
 
 };
